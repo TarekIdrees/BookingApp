4b74ed4554657ed00d59c9e37213b77d625a2f35
TarekIdrees <tarekidrees5@gmail.com>
2024-02-05 16:04:30 +0100

    initial configuration

---
+++ app/src/main/java/com/bookingapp/tareq/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.bookingapp.tareq.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Brand = Color(0xffE7F161)
+val White = Color(0xffF7FAFF)
+val Black80 = Color(0xff313131)
---
+++ app/src/main/java/com/bookingapp/tareq/ui/theme/Type.kt
@@ -0,0 +1,45 @@
+package com.bookingapp.tareq.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.bookingapp.tareq.R
+
+val RedHat = FontFamily(
+    listOf(
+        Font(R.font.red_hat_display_regular, FontWeight.Normal),
+        Font(R.font.red_hat_display_bold, FontWeight.Bold),
+        Font(R.font.red_hat_display_medium, FontWeight.Medium),
+        Font(R.font.red_hat_display_semi_bold, FontWeight.SemiBold)
+    )
+)
+
+val Typography = Typography(
+    titleSmall = TextStyle(
+        color = White,
+        fontSize = 16.sp,
+        fontFamily = RedHat,
+        fontWeight = FontWeight.SemiBold
+    ),
+    labelSmall = TextStyle(
+        color = White,
+        fontSize = 14.sp,
+        fontFamily = RedHat,
+        fontWeight = FontWeight.Bold
+    ),
+    displayLarge = TextStyle(
+        color = Brand,
+        fontSize = 28.sp,
+        fontFamily = RedHat,
+        fontWeight = FontWeight.SemiBold
+    ),
+    displaySmall = TextStyle(
+        color = White,
+        fontSize = 16.sp,
+        fontFamily = RedHat,
+        fontWeight = FontWeight.Bold
+    )
+)
