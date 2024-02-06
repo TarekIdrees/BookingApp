7ea29c12455c928fca904522dae971c0618876cc
TarekIdrees <tarekidrees5@gmail.com>
2024-02-06 15:53:31 +0100

    change system bar color

--- app/src/main/java/com/bookingapp/tareq/ui/theme/Theme.kt
+++ app/src/main/java/com/bookingapp/tareq/ui/theme/Theme.kt
@@ -48,7 +48,7 @@ fun BookingAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
