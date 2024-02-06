5891e8e3f495ee359adb263ceb5b49e015cc9ed1
TarekIdrees <tarekidrees5@gmail.com>
2024-02-06 15:57:22 +0100

    handel dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     kapt("com.google.dagger:hilt-compiler:2.44")
+
+    implementation ("androidx.compose.foundation:foundation:1.6.0")
+
 }
--- app/src/main/java/com/bookingapp/tareq/MainActivity.kt
+++ app/src/main/java/com/bookingapp/tareq/MainActivity.kt
@@ -3,23 +3,17 @@ package com.bookingapp.tareq
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import com.bookingapp.tareq.ui.presentation.flight_screen.FlightScreen
 import com.bookingapp.tareq.ui.theme.BookingAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookingAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                FlightScreen()
             }
         }
     }
