0b6fe175a10ea584c257406c8193698d96f3da01
TarekIdrees <tarekidrees5@gmail.com>
2024-02-07 17:39:22 +0100

    prepare ui state for screen

---
+++ app/src/main/java/com/bookingapp/tareq/ui/presentation/ticket_screen/TicketUiState.kt
@@ -0,0 +1,35 @@
+package com.bookingapp.tareq.ui.presentation.ticket_screen
+
+import com.bookingapp.tareq.data.Flight
+
+data class TicketUiState(
+    val locationShortcut: String = "",
+    val destinationShortcut: String = "",
+    val flightInformation: FlightInformation = FlightInformation()
+)
+
+data class FlightInformation(
+    val estimatedFlightTime: String = "",
+    val ticketPrice: String = "",
+    val departDate: String = "",
+    val gate: String = "",
+    val flightNumber: String = "",
+    val arriveDate: String = "",
+    val seatNumber: String = "",
+    val flightClass: String = "",
+    val barcodeImage: Int = 0
+)
+
+fun Flight.toFlightUiState(): FlightInformation{
+    return FlightInformation(
+        estimatedFlightTime = estimatedFlightTime,
+        ticketPrice = price,
+        departDate = departDate,
+        gate = gate,
+        flightNumber = flightNumber,
+        arriveDate = arriveDate,
+        seatNumber = seat,
+        flightClass = flightClass,
+        barcodeImage = barcode
+    )
+}
